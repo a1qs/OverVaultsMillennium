6e141dec5abaa51a61c2beff71cebb6937e6e99c
a1qs <ankiller2007.09.20@gmail.com>
2024-10-05 21:48:29 +0200

    Add PortalSavedData & PortalData to store valid structures

---
+++ src/main/java/io/iridium/overvaults/millenium/world/PortalData.java
@@ -0,0 +1,71 @@
+package io.iridium.overvaults.millenium.world;
+
+import io.iridium.overvaults.millenium.StructureSize;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Rotation;
+
+public class PortalData {
+    private final Rotation rotation;
+    private final BlockPos portalFrameCenterPos;
+    private final StructureSize size;
+    private final ResourceKey<Level> dimension;
+    private boolean activated;
+
+
+    public PortalData(Rotation rotation, BlockPos portalFrameCenterPos, StructureSize size, ResourceKey<Level> dimension, boolean activated) {
+        this.rotation = rotation;
+        this.portalFrameCenterPos = portalFrameCenterPos;
+        this.size = size;
+        this.activated = activated;
+        this.dimension = dimension;
+    }
+
+    public Rotation getRotation() {
+        return rotation;
+    }
+
+    public BlockPos getPortalFrameCenterPos() {
+        return portalFrameCenterPos;
+    }
+
+    public StructureSize getSize() {
+        return size;
+    }
+
+    public ResourceKey<Level> getDimension() {
+        return dimension;
+    }
+
+    public boolean getActiveState() {
+        return activated;
+    }
+
+    public void setActiveState(boolean state) {
+        activated = state;
+    }
+
+    //Portals that have different Active States will be considered equal
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+
+        PortalData data = (PortalData) obj;
+
+        if (!rotation.equals(data.rotation)) return false;
+        if (!portalFrameCenterPos.equals(data.portalFrameCenterPos)) return false;
+        if (!size.equals(data.size)) return false;
+        return dimension.equals(data.dimension);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = rotation.hashCode();
+        result = 31 * result + portalFrameCenterPos.hashCode();
+        result = 31 * result + size.hashCode();
+        result = 31 * result + dimension.hashCode();
+        return result;
+    }
+}
---
+++ src/main/java/io/iridium/overvaults/millenium/world/PortalSavedData.java
@@ -0,0 +1,89 @@
+package io.iridium.overvaults.millenium.world;
+
+import io.iridium.overvaults.millenium.StructureSize;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.saveddata.SavedData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PortalSavedData extends SavedData {
+    private static final String DATA_NAME = "overvaults_stored_structures";
+    private final List<PortalData> portalDataList = new ArrayList<>();
+
+    public void addPortalData(PortalData newPortalData) {
+        // Check if the new portal data is already in the list
+        boolean exists = portalDataList.stream().anyMatch(existingPortalData -> existingPortalData.equals(newPortalData));
+
+        if (!exists) {
+            portalDataList.add(newPortalData);
+            setDirty();
+        }
+    }
+
+    public List<PortalData> getPortalData() {
+        return portalDataList;
+    }
+
+    public boolean hasActiveOverVault() {
+        return portalDataList.stream().anyMatch(PortalData::getActiveState);
+    }
+
+
+    public PortalData getFirstActivePortalData() {
+        return portalDataList.stream()
+                .filter(PortalData::getActiveState)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static PortalSavedData load(CompoundTag nbt) {
+        PortalSavedData data = new PortalSavedData();
+        ListTag listTag = nbt.getList("PortalDataList", Tag.TAG_COMPOUND);
+
+        for (int i = 0; i < listTag.size(); i++) {
+            CompoundTag portalTag = listTag.getCompound(i);
+
+            Rotation rotation = Rotation.valueOf(portalTag.getString("Rotation"));
+            BlockPos portalFrameCenterPos = BlockPos.of(portalTag.getLong("PortalFrameCenterPos"));
+            StructureSize size = StructureSize.valueOf(portalTag.getString("Size"));
+            ResourceKey<Level> dimension = ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(portalTag.getString("Dimension")));
+            boolean activeState = portalTag.getBoolean("ActiveState");
+
+            data.addPortalData(new PortalData(rotation, portalFrameCenterPos, size, dimension, activeState));
+        }
+        return data;
+    }
+
+    @Override
+    public CompoundTag save(CompoundTag nbt) {
+        ListTag listTag = new ListTag();
+
+        for (PortalData data : portalDataList) {
+            CompoundTag portalTag = new CompoundTag();
+            portalTag.putString("Rotation", data.getRotation().name());
+            portalTag.putLong("PortalFrameCenterPos", data.getPortalFrameCenterPos().asLong());
+            portalTag.putString("Size", data.getSize().name());
+            portalTag.putString("Dimension", data.getDimension().location().toString());
+            portalTag.putBoolean("ActiveState", data.getActiveState());
+
+            listTag.add(portalTag);
+        }
+        nbt.put("PortalDataList", listTag);
+        return nbt;
+    }
+
+    public static PortalSavedData get(ServerLevel world) {
+        return world.getDataStorage().computeIfAbsent(PortalSavedData::load, PortalSavedData::new, DATA_NAME);
+    }
+}
