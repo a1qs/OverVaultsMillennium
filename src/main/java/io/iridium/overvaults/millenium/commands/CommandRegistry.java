9436540318c7b6c674fe881146c52de8fe45b19d
a1qs <ankiller2007.09.20@gmail.com>
2024-10-27 14:30:02 +0100

    Register all new Commands

--- src/main/java/io/iridium/overvaults/millenium/commands/CommandRegistry.java
+++ src/main/java/io/iridium/overvaults/millenium/commands/CommandRegistry.java
@@ -1,18 +1,35 @@
 package io.iridium.overvaults.millenium.commands;
 
+import com.mojang.brigadier.CommandDispatcher;
 import io.iridium.overvaults.OverVaults;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.server.command.ConfigCommand;
+
+import java.util.function.Supplier;
 
 @Mod.EventBusSubscriber(modid = OverVaults.MOD_ID)
 public class CommandRegistry {
+
     @SubscribeEvent
     public static void onCommandsRegister(RegisterCommandsEvent event) {
-        new StructureCommand(event.getDispatcher());
-
-        ConfigCommand.register(event.getDispatcher());
+        registerCommands(event.getDispatcher());
     }
+
+    public static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher) {
+        registerCommand(StructureCommands::new, dispatcher);
+        registerCommand(DebugCommands::new, dispatcher);
+        registerCommand(InfoCommands::new, dispatcher);
+
+        registerCommand(LootTableCommands::new, dispatcher);
+    }
+
+    public static <T extends BaseCommand> T registerCommand(Supplier<T> supplier, CommandDispatcher<CommandSourceStack> dispatcher) {
+        T command = supplier.get();
+        command.registerCommand(dispatcher);
+        return command;
+    }
+
 
 }
