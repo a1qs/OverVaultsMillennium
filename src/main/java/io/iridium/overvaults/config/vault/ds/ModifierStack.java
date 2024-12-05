19a38d3aa19475c8a644d6c67f4af34686659dbe
a1qs <ankiller2007.09.20@gmail.com>
2024-12-05 21:34:14 +0100

    fix ModifierStack class to not throw an exception

--- src/main/java/io/iridium/overvaults/config/vault/ds/ModifierStack.java
+++ src/main/java/io/iridium/overvaults/config/vault/ds/ModifierStack.java
@@ -4,5 +4,22 @@ import com.google.gson.annotations.Expose;
 import net.minecraft.resources.ResourceLocation;
 
 
-public record ModifierStack(@Expose ResourceLocation modifierId, @Expose int amount) {
+public class ModifierStack {
+    @Expose
+    private ResourceLocation modifierId;
+    @Expose
+    private int amount;
+
+    public ModifierStack(ResourceLocation modifierId, int amount) {
+        this.modifierId = modifierId;
+        this.amount = amount;
+    }
+
+    public ResourceLocation getModifierId() {
+        return modifierId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
