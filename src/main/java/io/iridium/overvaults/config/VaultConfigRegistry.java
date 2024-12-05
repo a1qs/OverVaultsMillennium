55daf76c1365e306f56bb91696c228c24eda2076
a1qs <ankiller2007.09.20@gmail.com>
2024-12-05 23:23:01 +0100

    register new mob config

--- src/main/java/io/iridium/overvaults/config/VaultConfigRegistry.java
+++ src/main/java/io/iridium/overvaults/config/VaultConfigRegistry.java
@@ -1,15 +1,18 @@
 package io.iridium.overvaults.config;
 
 import io.iridium.overvaults.config.vault.OverVaultsGeneralConfig;
+import io.iridium.overvaults.config.vault.OverVaultsMobConfig;
 import io.iridium.overvaults.config.vault.OverVaultsPortalConfig;
 
 public class VaultConfigRegistry {
     public static OverVaultsPortalConfig OVERVAULTS_PORTAL_CONFIG;
     public static OverVaultsGeneralConfig OVERVAULTS_GENERAL_CONFIG;
+    public static OverVaultsMobConfig OVERVAULTS_MOB_CONFIG;
 
 
     public static void registerCustomConfigs() {
         OVERVAULTS_PORTAL_CONFIG = new OverVaultsPortalConfig().readConfig();
         OVERVAULTS_GENERAL_CONFIG = new OverVaultsGeneralConfig().readConfig();
+        OVERVAULTS_MOB_CONFIG = new OverVaultsMobConfig().readConfig();
     }
 }
