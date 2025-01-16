5debbe69669f13abcfe8e14d20dfaaf081ba05d3
a1qs <ankiller2007.09.20@gmail.com>
2025-01-16 22:25:58 +0100

    update OverVaultsPortalConfig to use PortalEntry instead of a List of modifier stack entries

--- src/main/java/io/iridium/overvaults/config/vault/OverVaultsPortalConfig.java
+++ src/main/java/io/iridium/overvaults/config/vault/OverVaultsPortalConfig.java
@@ -3,26 +3,26 @@ package io.iridium.overvaults.config.vault;
 
 
 import com.google.gson.annotations.Expose;
+import com.mojang.datafixers.util.Pair;
 import io.iridium.overvaults.OverVaults;
 import io.iridium.overvaults.config.VaultConfigRegistry;
+import io.iridium.overvaults.config.vault.entry.CrystalDataEntry;
 import io.iridium.overvaults.config.vault.entry.ModifierStackEntry;
+import io.iridium.overvaults.config.vault.entry.PortalEntry;
+import io.iridium.overvaults.mixin.accessor.AccessorPoolCrystalTheme;
 import iskallia.vault.VaultMod;
 import iskallia.vault.config.Config;
-import iskallia.vault.core.random.JavaRandom;
-import iskallia.vault.core.vault.modifier.VaultModifierStack;
-import iskallia.vault.core.vault.modifier.registry.VaultModifierRegistry;
-import iskallia.vault.core.vault.modifier.spi.VaultModifier;
-import iskallia.vault.init.ModConfigs;
-import iskallia.vault.init.ModItems;
+import iskallia.vault.core.world.roll.IntRoll;
 import iskallia.vault.item.crystal.CrystalData;
-import iskallia.vault.item.crystal.model.RawCrystalModel;
-import iskallia.vault.item.crystal.objective.EmptyCrystalObjective;
+import iskallia.vault.item.crystal.layout.ClassicInfiniteCrystalLayout;
+import iskallia.vault.item.crystal.objective.BingoCrystalObjective;
+import iskallia.vault.item.crystal.objective.ScavengerCrystalObjective;
 import iskallia.vault.item.crystal.theme.PoolCrystalTheme;
 import iskallia.vault.item.crystal.theme.ValueCrystalTheme;
+import iskallia.vault.item.crystal.time.ValueCrystalTime;
 import iskallia.vault.util.data.WeightedList;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 import java.util.ArrayList;
@@ -32,10 +32,9 @@ import java.util.Random;
 public class OverVaultsPortalConfig extends Config {
 
     private static final Random RANDOM = new Random();
-    @Expose public float CHANCE_OF_RAW_VAULT;
     @Expose public float CHANCE_OF_NETHER_THEME_IN_NETHER;
     @Expose public float CHANCE_OF_VOID_THEME_IN_END;
-    @Expose public WeightedList<List<ModifierStackEntry>> MODIFIER_LISTS = new WeightedList<>();
+    @Expose public WeightedList<PortalEntry> PORTAL_LIST = new WeightedList<>();
     @Expose public List<ResourceLocation> NETHER_VAULT_THEMES = new ArrayList<>();
     @Expose public List<ResourceLocation> VOID_VAULT_THEMES = new ArrayList<>();
 
@@ -46,38 +45,32 @@ public class OverVaultsPortalConfig extends Config {
 
     @Override
     protected void reset() {
-        CHANCE_OF_RAW_VAULT = 0.75F;
-
         CHANCE_OF_NETHER_THEME_IN_NETHER = 0.10F;
         CHANCE_OF_VOID_THEME_IN_END = 0.075F;
 
-        MODIFIER_LISTS.add(
-                List.of(
-                        new ModifierStackEntry(new ResourceLocation("the_vault:energizing"), 1),
-                        new ModifierStackEntry(new ResourceLocation("the_vault:soul_boost"), 1),
-                        new ModifierStackEntry(new ResourceLocation("the_vault:item_quantity"), 3),
-                        new ModifierStackEntry(new ResourceLocation("the_vault:item_rarity"), 3)
-                ),
-                75
+        CrystalDataEntry entry0 = new CrystalDataEntry(
+                new BingoCrystalObjective(),
+                new ClassicInfiniteCrystalLayout(1),
+                new PoolCrystalTheme(VaultMod.id("raw")),
+                List.of(new ModifierStackEntry(VaultMod.id("item_quantity"), 16), new ModifierStackEntry(VaultMod.id("soul_boost"), 64)),
+                new ValueCrystalTime(IntRoll.ofConstant(1800)),
+                20,
+                false
+                );
+
+        PORTAL_LIST.add(new PortalEntry(entry0, "overvaults.portal.tier.s", true), 20);
+
+        CrystalDataEntry entry1 = new CrystalDataEntry(
+                new ScavengerCrystalObjective(0.6F),
+                new ClassicInfiniteCrystalLayout(1),
+                new PoolCrystalTheme(VaultMod.id("raw")),
+                List.of(new ModifierStackEntry(VaultMod.id("energizing"), 16), new ModifierStackEntry(VaultMod.id("item_quantity"), 16)),
+                new ValueCrystalTime(IntRoll.ofConstant(3600)),
+                100,
+                false
         );
 
-        MODIFIER_LISTS.add(
-                List.of(
-                        new ModifierStackEntry(new ResourceLocation("the_vault:prosperous"), 1),
-                        new ModifierStackEntry(new ResourceLocation("the_vault:haunted"), 3),
-                        new ModifierStackEntry(new ResourceLocation("the_vault:coin_pile"), 3),
-                        new ModifierStackEntry(new ResourceLocation("the_vault:gilded"), 3),
-                        new ModifierStackEntry(new ResourceLocation("the_vault:ornate"), 3),
-                        new ModifierStackEntry(new ResourceLocation("the_vault:living"), 3)
-                ),
-                20
-        );
-        MODIFIER_LISTS.add(
-                List.of(
-                        new ModifierStackEntry(new ResourceLocation("the_vault:prosperous"), 25)
-                ),
-                5
-        );
+        PORTAL_LIST.add(new PortalEntry(entry1, "overvaults.portal.tier.splusplus", true), 2);
 
         NETHER_VAULT_THEMES.add(new ResourceLocation("the_vault:classic_vault_nether_crimson"));
         NETHER_VAULT_THEMES.add(new ResourceLocation("the_vault:classic_vault_nether_warped"));
@@ -90,47 +83,49 @@ public class OverVaultsPortalConfig extends Config {
     }
 
 
-    public static CrystalData getRandomCrystalData(ResourceKey<Level> dimension) {
+    public static Pair<PortalEntry, CrystalData> getRandomCrystalData(ResourceKey<Level> dimension) {
         OverVaultsPortalConfig cfg = VaultConfigRegistry.OVERVAULTS_PORTAL_CONFIG;
 
-        ItemStack crystalItem = new ItemStack(ModItems.VAULT_CRYSTAL);
-        CrystalData crystal = CrystalData.read(crystalItem);
-        crystal.getProperties().setLevel(0);
+        CrystalData crystal = CrystalData.empty();
 
-        if(RANDOM.nextFloat() <= cfg.CHANCE_OF_RAW_VAULT) {
-            crystal.setModel(new RawCrystalModel());
-            crystal.setObjective(new EmptyCrystalObjective());
-            crystal.setTheme(new PoolCrystalTheme(VaultMod.id("raw")));
+        PortalEntry portalEntry = cfg.PORTAL_LIST.getRandom(Config.rand);
+        if (portalEntry != null) {
+            CrystalDataEntry.applyData(portalEntry.getCrystalData(), crystal);
+            System.out.println(portalEntry.getCrystalData().getTheme().equals(new PoolCrystalTheme(VaultMod.id("raw"))));
+            ResourceLocation curTheme = null;
 
-            for (VaultModifier<?> modifier : ModConfigs.VAULT_MODIFIER_POOLS.getRandom(VaultMod.id("raw"), 0, JavaRandom.ofNanoTime())) {
-                crystal.getModifiers().add(VaultModifierStack.of(modifier, 1));
+            if(portalEntry.getCrystalData().getTheme() instanceof PoolCrystalTheme theme) {
+                curTheme = ((AccessorPoolCrystalTheme) theme).getId();
             }
 
-            crystal.getModifiers().setRandomModifiers(false);
-            return crystal;
-        }
+            if(dimension.equals(Level.NETHER) && RANDOM.nextFloat() <= cfg.CHANCE_OF_NETHER_THEME_IN_NETHER) {
 
-
-        List<ModifierStackEntry> modifierStackEntries = cfg.MODIFIER_LISTS.getRandom(Config.rand);
-        if (modifierStackEntries != null) {
-            for(ModifierStackEntry stack : modifierStackEntries) {
-                crystal.getModifiers().add(VaultModifierStack.of(VaultModifierRegistry.get(stack.getModifierId()), stack.getAmount()));
+                if(!(curTheme != null && curTheme.equals(VaultMod.id("raw")))) {
+                    crystal.setTheme(new ValueCrystalTheme(
+                            cfg.NETHER_VAULT_THEMES.get(RANDOM.nextInt(cfg.NETHER_VAULT_THEMES.size()))
+                    ));
+                }
+            } else if (dimension.equals(Level.END) && RANDOM.nextFloat() <= cfg.CHANCE_OF_VOID_THEME_IN_END) {
+                if(!(curTheme != null && curTheme.equals(VaultMod.id("raw")))) {
+                    crystal.setTheme(new ValueCrystalTheme(
+                            cfg.NETHER_VAULT_THEMES.get(RANDOM.nextInt(cfg.NETHER_VAULT_THEMES.size()))
+                    ));
+                }
+                if(!(curTheme != null && curTheme.equals(VaultMod.id("raw")))) {
+                    crystal.setTheme(new ValueCrystalTheme(
+                            cfg.VOID_VAULT_THEMES.get(RANDOM.nextInt(cfg.VOID_VAULT_THEMES.size()))
+                    ));
+                }
             }
+
+
         } else {
-            OverVaults.LOGGER.error("Found invalid modifier list inside of '{}' config!", VaultConfigRegistry.OVERVAULTS_PORTAL_CONFIG.getName());
+            OverVaults.LOGGER.error("Found invalid portal data inside of '{}' config!", VaultConfigRegistry.OVERVAULTS_PORTAL_CONFIG.getName());
         }
 
 
-        if(dimension.equals(Level.NETHER) && RANDOM.nextFloat() <= cfg.CHANCE_OF_NETHER_THEME_IN_NETHER) {
-            crystal.setTheme(new ValueCrystalTheme(
-                    cfg.NETHER_VAULT_THEMES.get(RANDOM.nextInt(cfg.NETHER_VAULT_THEMES.size()))
-            ));
-        } else if (dimension.equals(Level.END) && RANDOM.nextFloat() <= cfg.CHANCE_OF_VOID_THEME_IN_END) {
-            crystal.setTheme(new ValueCrystalTheme(
-                    cfg.VOID_VAULT_THEMES.get(RANDOM.nextInt(cfg.VOID_VAULT_THEMES.size()))
-            ));
-        }
 
-        return crystal;
+
+        return Pair.of(portalEntry, crystal);
     }
 }
